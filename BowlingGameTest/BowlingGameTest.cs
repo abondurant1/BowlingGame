52d14e66e2272529f491356540c4b9273031ec54
Bondurant <AMBONDU@internal.local>
2017-06-15 16:03:49 -0400

    first commit

---
+++ BowlingGameTest/BowlingGameTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingGame;
+
+namespace BowlingGameTest
+{
+    [TestClass]
+    public class BowlingGameTest
+    {
+        private Game bowlingGame;
+
+        [TestInitialize]
+
+        public void Setup()
+        {
+            bowlingGame = new Game();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ BowlingGameTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BowlingGameTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BowlingGameTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ac71a266-62c4-4aef-b002-d06f3503bc3f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
