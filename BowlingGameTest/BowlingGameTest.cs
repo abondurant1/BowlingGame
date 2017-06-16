2052342e288a7d7703e54670a1caa45452a453ab
Bondurant <abondurant1@radford.edu>
2017-06-16 17:09:30 -0400

    Final Test

--- BowlingGameTest/BowlingGameTest.cs
+++ BowlingGameTest/BowlingGameTest.cs
@@ -54,6 +54,13 @@ namespace BowlingGameTest
             Assert.AreEqual(24, g.Score());
         }
 
+        [TestMethod]
+        public void TestPerfectGame()
+        {
+            RollMany(12, 10);
+            Assert.AreEqual(300, g.Score());
+        }
+
         private void RollStrike()
         {
             g.roll(10);
